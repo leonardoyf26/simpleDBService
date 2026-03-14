5608d7b936e26bc5b2f9964fba1f848725513954
leonardoyf26 <leonardoyf26@gmail.com>
2026-03-13 21:24:07 -0300

    refactor: improve parser structure

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,17 +1,26 @@
 #include "parser.h"
 #include <cctype>
 #include <algorithm>
-#include <iostream>
+#include <unordered_map>
 
 namespace simpledb
 {
+
+static const std::unordered_map<std::string, CommandType> commandMap =
+{
+    {"SET", CommandType::SET},
+    {"GET", CommandType::GET},
+    {"DEL", CommandType::DEL},
+    {"EXISTS", CommandType::EXISTS},
+    {"EXIT", CommandType::EXIT}
+};
 
 Command Parser::parse(const std::string& input)
 {
     auto tokens = tokenize(input);
 
     if (tokens.empty())
-        return buildCommand(tokens, CommandType::UNKNOWN);
+        return Command{};
 
     CommandType type = getCommandType(tokens[0]);
     return buildCommand(tokens, type);
@@ -26,7 +35,9 @@ std::vector<std::string> Parser::tokenize(const std::string& input) const
 
     while ((end = input.find(' ', start)) != std::string::npos)
     {
-        tokens.push_back(input.substr(start, end - start));
+        if (end > start)
+            tokens.push_back(input.substr(start, end - start));
+
         start = end + 1;
     }
 
@@ -44,18 +55,18 @@ CommandType Parser::getCommandType(const std::string& token) const
         upper.begin(),
         upper.end(),
         upper.begin(),
-        [](unsigned char c) { return std::toupper(c); });
+        [](unsigned char c){ return std::toupper(c); }
+    );
 
-    if (upper == "SET")    return CommandType::SET;
-    if (upper == "GET")    return CommandType::GET;
-    if (upper == "DEL")    return CommandType::DEL;
-    if (upper == "EXISTS") return CommandType::EXISTS;
-    if (upper == "EXIT")   return CommandType::EXIT;
+    auto it = commandMap.find(upper);
+
+    if (it != commandMap.end())
+        return it->second;
 
     return CommandType::UNKNOWN;
 }
 
-Command Parser::buildCommand(const std::vector<std::string>& tokens, CommandType type)
+Command Parser::buildCommand(const std::vector<std::string>& tokens, CommandType type) const
 {
     Command cmd;
     cmd.type = type;
@@ -63,21 +74,34 @@ Command Parser::buildCommand(const std::vector<std::string>& tokens, CommandType
     switch (type)
     {
         case CommandType::SET:
-            if (tokens.size() >= 3)
+            if (tokens.size() == 3)
             {
                 cmd.key = tokens[1];
                 cmd.value = tokens[2];
+            }
+            else
+            {
+                cmd.type = CommandType::UNKNOWN;
             }
             break;
 
         case CommandType::GET:
         case CommandType::DEL:
         case CommandType::EXISTS:
-            if (tokens.size() >= 2)
+            if (tokens.size() == 2)
+            {
                 cmd.key = tokens[1];
+            }
+            else
+            {
+                cmd.type = CommandType::UNKNOWN;
+            }
             break;
 
         case CommandType::EXIT:
+            if (tokens.size() != 1)
+                cmd.type = CommandType::UNKNOWN;
+            break;
         case CommandType::UNKNOWN:
         default:
             break;
